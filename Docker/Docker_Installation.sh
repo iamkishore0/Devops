c559f50ab3187bea7dc6c4092c192403ce33b10d
Devops_life <98376417+devopslife999@users.noreply.github.com>
2022-07-18 20:33:17 +0530

    Create Docker_Installation.sh

---
+++ Docker/Docker_Installation.sh
@@ -0,0 +1,11 @@
+#Install docker in centos
+sudo yum install -y yum-utils &&
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo &&
+    
+    
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin &&
+
+sudo systemctl start docker 
